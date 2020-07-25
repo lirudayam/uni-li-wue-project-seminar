4b38e1f3435f0ceb136d2e58056f8627988ead38
Leo Irudayam <leo.richard.irudayam@sap.com>
2020-07-25 09:44:57 +0200

    new mta version

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ cd ..
 cp -R dw-sac-middleware/gen/srv/srv/csn.json dw-kafka-processor/csn.json
 cf stop uni-li-wue-dw-kafka-processor
 cd dw-sac-middleware 
-cf deploy mta_archives/uni-li-wue-dw-cloud_1.0.2.mtar
+cf deploy mta_archives/uni-li-wue-dw-cloud_1.2.mtar
 cd ../dw-kafka-processor
 cf push
