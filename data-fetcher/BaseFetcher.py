96242c3133018df26c13f947dacb409069caf293
Leo Irudayam <leo.richard.irudayam@sap.com>
2020-08-17 10:07:13 +0200

    fixed issue of recursion errors

--- data-fetcher/BaseFetcher.py
+++ data-fetcher/BaseFetcher.py
@@ -3,7 +3,11 @@ import threading
 
 from DWConfigs import DWConfigs
 
-logging.basicConfig(filename='output.log', level=logging.INFO)
+logging.basicConfig(
+    filename='output.log',
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 class BaseFetcher:
@@ -13,9 +17,12 @@ class BaseFetcher:
         self._health_ping_fn = health_ping_fn
         self._process_fn = process_fn
 
+        self._health_interval = DWConfigs().get_health_ping_interval(self._kafka_topic)
+        self._app_interval = DWConfigs().get_fetch_interval(self._kafka_topic)
+
         # define the base timers
-        self.health_timer = threading.Timer(DWConfigs().get_health_ping_interval(kafka_topic), health_ping_fn, [], {})
-        self.app_timer = threading.Timer(DWConfigs().get_fetch_interval(kafka_topic), process_fn, [], {})
+        self.health_timer = threading.Timer(self._health_interval, self._health_ping_fn, [], {})
+        self.app_timer = threading.Timer(self._app_interval, self._process_fn, [], {})
         logging.info('Successful init')
 
         # fire threads
@@ -23,13 +30,15 @@ class BaseFetcher:
         self.app_timer.start()
 
     def run_health(self):
-        self.health_timer.cancel()
-        self.health_timer = threading.Timer(
-            DWConfigs().get_health_ping_interval(self._kafka_topic), self._health_ping_fn, [], {})
-        self.health_timer.start()
+        if DWConfigs().get_health_ping_interval(self._kafka_topic) != self._health_interval:
+            self.health_timer.cancel()
+            self._health_interval = DWConfigs().get_health_ping_interval(self._kafka_topic)
+            self.health_timer = threading.Timer(self._health_interval, self._health_ping_fn, [], {})
+            self.health_timer.start()
 
     def run_app(self):
-        self.app_timer.cancel()
-        self.app_timer = threading.Timer(
-            DWConfigs().get_fetch_interval(self._kafka_topic), self._process_fn, [], {})
-        self.app_timer.start()
+        if DWConfigs().get_fetch_interval(self._kafka_topic) != self._app_interval:
+            self.app_timer.cancel()
+            self.app_timer = threading.Timer(
+                DWConfigs().get_fetch_interval(self._kafka_topic), self._process_fn, [], {})
+            self.app_timer.start()
